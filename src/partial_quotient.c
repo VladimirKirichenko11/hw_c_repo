6ac9ac91caf2374a5c2ac33386b1d5230515be91
Vladimir Kirichenko <vova.kirichenko.1973@bk.ru>
2025-10-27 11:17:01 +0300

    Add a finding of partial quotient

---
+++ src/partial_quotient.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int a, b, quot = 0;
+	printf("Enter first number: ");
+	scanf_s("%d", &a);
+	printf("Enter second number: ");
+	scanf_s("%d", &b);
+	int max, min;
+	// Выбор большего числа в качестве делимого, меньшего - делителя
+	if (abs(a) >= abs(b))
+		max = a, min = b;
+	else
+		max = b, min = a;
+	if (min == 0) {
+		printf("Error: division by zero\n");
+
+		return 1;
+	}
+	// Если вводятся числа с разными знаками
+	if ((a > 0 && b < 0) || (b > 0 && a < 0)) {
+		while (abs(max) - abs(min) >= 0) {
+			max = -(abs(max) - abs(min));
+			quot--;
+		}
+	}
+	else {
+		while (abs(max) - abs(min) >= 0) {
+			max -= min;
+			quot++;
+		}
+	}
+	printf("Quotient = %d\n", quot);
+	printf("Remainder = %d\n", max);
+
+	return 0;
+}
