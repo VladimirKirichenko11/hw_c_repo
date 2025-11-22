a43b6e5b7261fc22b8293356eed8490406594cb2
Vladimir Kirichenko <vova.kirichenko.1973@bk.ru>
2025-11-22 17:48:33 +0300

    Add an app code

---
+++ src/main.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+extern int sort_array(int* array, int size);
+
+int main() {
+	int numbers[100];
+	int count = 0;
+	printf("Enter numbers: \n");
+	while (count < 100 && scanf("%d", &numbers[count]) == 1)
+		count++;
+	printf("Before sorting: ");
+	for (int i = 0; i < count; i++)
+		printf("%d ", numbers[i]);
+	printf("\n");
+	int moved_count = sort_array(numbers, count);
+	printf("After sorting: ");
+	for (int i = 0; i < count; i++)
+		printf("%d ", numbers[i]);
+	printf("\n");
+
+	return moved_count;
+}
