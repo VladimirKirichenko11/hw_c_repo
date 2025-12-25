1a1c105af37363ea73b00d502ad2d97c4f1b2ab6
Vladimir Kirichenko <vova.kirichenko.1973@bk.ru>
2025-12-25 14:07:14 +0300

    Add a simple bracket balance

---
+++ src/bracket_balance.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main() {
+	char str[100];
+	int balance = 0;
+	printf("Enter the string: ");
+	fgets(str, sizeof(str), stdin); // Reading the string
+	for (int i = 0; str[i] != '\0'; i++) {
+		if (str[i] == '(')
+			balance++;
+		else if (str[i] == ')')
+			balance--;
+		if (balance < 0) {
+			printf("Error, incorrect bracket balance\n");
+			break;
+		}
+
+	}
+	if (balance > 0)
+		printf("Error, incorrect bracket balance\n");
+	else
+		printf("Bracket balance is correct\n");
+
+	return 0;
+}
