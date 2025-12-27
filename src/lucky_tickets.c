d383918897ea27ae6381d2ad0235b61146f9f25f
Vladimir Kirichenko <vova.kirichenko.1973@bk.ru>
2025-12-28 01:48:50 +0300

    Add a correct implementation of lucky tickets programm

--- src/lucky_tickets.c
+++ src/lucky_tickets.c
@@ -1,24 +1,18 @@
 ﻿#include <stdio.h>
 
 int main() {
-	int number[6];
-	int counter = 0;
-	// Получаем номера билетов с помощью вложенных циклов
-	for (number[0] = 0; number[0] <= 9; number[0]++) {
-		for (number[1] = 0; number[1] <= 9; number[1]++) {
-			for (number[2] = 0; number[2] <= 9; number[2]++) {
-				for (number[3] = 0; number[3] <= 9; number[3]++) {
-					for (number[4] = 0; number[4] <= 9; number[4]++) {
-						for (number[5] = 0; number[5] <= 9; number[5]++) {
-							if (number[0] + number[1] + number[2] == number[3] + number[4] + number[5])
-								counter++;
-						}
-					}
-				}
-			}
-		}
-	}
-	printf("%d\n", counter);
+    int total = 0;
+    int count[28] = { 0 };
+    for (int a = 0; a < 10; a++) {
+        for (int b = 0; b < 10; b++) {
+            for (int c = 0; c < 10; c++) {
+                count[a + b + c]++;
+            }
+        }
+    }
+    for (int sum = 0; sum < 28; sum++)
+            total += count[sum] * count[sum];
+    printf("Quantity of lucky tickets = %d\n", total);
 
 	return 0;
 }
